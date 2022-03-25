4f6024a2896b111f6a39425a5dc0664d6820f1da
xiefeng <1294699027@qq.com>
2022-03-25 17:46:52 +0800

    🐛fix: fix import node_modules bug

--- loader.mjs
+++ loader.mjs
@@ -1,10 +1,11 @@
 import { extname } from 'path'
-import { builtinModules as builtin } from 'module'
 
 // --experimental-loader
 
 export async function resolve(specifier, context, defaultResolve) {
-  if (!builtin.includes(specifier) && extname(specifier) !== '.js') {
+
+  // start with / ./ ../
+  if (/^[\/\.]/.test(specifier) && extname(specifier) !== '.js') {
     specifier = specifier + '.js'
   }
   return defaultResolve(specifier, context, defaultResolve)
